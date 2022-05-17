d7f37e7a839b864f3d70ba0a79a852f558bc8972
zenjbara <adam.benjbara@zenika.com>
2022-05-17 16:16:56 +0200

    should_throw_exception_when_command_id_unknown : OK

--- src/main/java/com/zenika/business/impl/RoverBusinessImpl.java
+++ src/main/java/com/zenika/business/impl/RoverBusinessImpl.java
@@ -1,6 +1,7 @@
 package com.zenika.business.impl;
 
 import com.zenika.business.RoverBusiness;
+import com.zenika.common.RoverException;
 import com.zenika.domain.Rover;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ public class RoverBusinessImpl implements RoverBusiness {
     private Rover rover;
 
     @Override
-    public void receiveSingleCommand(char command) throws Exception {
+    public void receiveSingleCommand(char command) throws RoverException {
         switch (Character.toUpperCase(command)){
             case 'F':
                 rover.moveForward();
@@ -27,7 +28,7 @@ public class RoverBusinessImpl implements RoverBusiness {
                 rover.turnLeft();
                 break;
             default:
-                throw new Exception("unknown command :" + command);
+                throw new RoverException("unknown command :" + command);
         }
     }
 }
